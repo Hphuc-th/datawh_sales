fece94ffddde230d954f432f0e97f9c38473aafe
Hphuc-th <tthongphuc.work@gmail.com>
2026-07-17 18:02:31 +0700

    Fix print statements for consistency in load_bronze.sql

--- script/procedure/load_bronze.sql
+++ script/procedure/load_bronze.sql
@@ -3,10 +3,10 @@ BEGIN
 	BEGIN TRY 
 		DECLARE @batch_start_time DATETIME, @batch_end_time DATETIME, @start_time DATETIME,@end_time DATETIME
 		SET @batch_start_time = SYSDATETIME()
-		PRINT (' Start loading data into bronze layer at'+ CONVERT(VARCHAR(30), @batch_start_time, 120));
+		PRINT ('Start loading data into bronze layer at'+ CONVERT(VARCHAR(30), @batch_start_time, 120));
 		PRINT ('=============================================================');
 
-		PRINT (' Inserting data into : bronze.erp_cust_az12');
+		PRINT ('Inserting data into : bronze.erp_cust_az12');
 		SET @start_time= SYSDATETIME();
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		BULK INSERT bronze.erp_cust_az12
@@ -17,7 +17,7 @@ BEGIN
 		SET @end_time= SYSDATETIME();
 		PRINT ('Load duration: '+ CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) +'seconds');
 
-		PRINT (' Inserting data into : bronze.erp_loc_a101');
+		PRINT ('Inserting data into : bronze.erp_loc_a101');
 		SET @start_time= SYSDATETIME();
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		BULK INSERT bronze.erp_loc_a101
@@ -28,7 +28,7 @@ BEGIN
 		SET @end_time= SYSDATETIME();
 		PRINT ('Load duration: '+ CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) +'seconds');
 
-		PRINT (' Inserting data into : bronze.erp_px_cat_g1v2');
+		PRINT ('Inserting data into : bronze.erp_px_cat_g1v2');
 		SET @start_time= SYSDATETIME();
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		BULK INSERT bronze.erp_px_cat_g1v2
@@ -40,7 +40,7 @@ BEGIN
 		PRINT ('Load duration: '+ CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) +'seconds');
 
 		PRINT '==========================================' 
-		PRINT (' Inserting data into : bronze.crm_cust_info');
+		PRINT ('Inserting data into : bronze.crm_cust_info');
 		SET @start_time= SYSDATETIME();
 		TRUNCATE TABLE bronze.crm_cust_info;
 		BULK INSERT bronze.crm_cust_info
@@ -51,7 +51,7 @@ BEGIN
 		SET @end_time= SYSDATETIME();
 		PRINT ('Load duration: '+ CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) +'seconds');
 
-		PRINT (' Inserting data into : bronze.crm_prd_info');
+		PRINT ('Inserting data into : bronze.crm_prd_info');
 		SET @start_time= SYSDATETIME();
 		TRUNCATE TABLE bronze.crm_prd_info;
 		BULK INSERT bronze.crm_prd_info
@@ -62,7 +62,7 @@ BEGIN
 		SET @end_time= SYSDATETIME();
 		PRINT ('Load duration: '+ CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) +'seconds');
 
-		PRINT (' Inserting data into : bronze.crm_sales_details');
+		PRINT ('Inserting data into : bronze.crm_sales_details');
 		SET @start_time= SYSDATETIME();
 		TRUNCATE TABLE bronze.crm_sales_details;
 		BULK INSERT bronze.crm_sales_details
@@ -72,7 +72,7 @@ BEGIN
 				FORMAT ='CSV');
 		SET @end_time= SYSDATETIME();
 		PRINT ('Load duration: '+ CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) +'seconds');
-		PRINT (' Batch load duration '+ CAST(DATEDIFF(SECOND,@start_time, @end_time) AS NVARCHAR) +'seconds');
+		PRINT ('Batch load duration '+ CAST(DATEDIFF(SECOND,@start_time, @end_time) AS NVARCHAR) +'seconds');
 	END TRY
 	BEGIN CATCH
 		PRINT '=========================================='
@@ -83,5 +83,6 @@ BEGIN
 		PRINT '=========================================='
 	END CATCH
 END
-
+GO
+EXEC bronze.load_bronze; 
 
